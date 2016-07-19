942d49e20f916dbe4b3384a283f9b1ddcedae663
Nurdin <spaikerkg@yahoo.com>
2016-07-19 18:58:34 +0600

    Added dashboard

--- js/main.js
+++ js/main.js
@@ -49,8 +49,39 @@ $( document ).ready(function() {
         colors: [
             '#52dc90',
             '#55a8fd',
-            '#718bb7',
+            '#718bb7'
         ],
         formatter: function (x) { return x + "%"}
+    });
+
+    $(".dropdown-toggle").click(function(){
+        $(".dropdown-menu").toggleClass("dropdown-menu_show");
+    });
+
+    Morris.Area({
+        element: 'graph-area',
+        data: [
+            {x: '2010 Q4', y: 3, z: 7},
+            {x: '2011 Q1', y: 3, z: 4},
+            {x: '2011 Q2', y: null, z: 1},
+            {x: '2011 Q3', y: 2, z: 5},
+            {x: '2011 Q4', y: 8, z: 2},
+            {x: '2012 Q1', y: 4, z: 4}
+        ],
+        xkey: 'x',
+        ykeys: ['y', 'z'],
+        labels: ['ONLINE', 'OFFLINE'],
+        continuousLine: false,
+        fillOpacity: 0.2,
+        lineColors: ['#ea5761', '#52dc90'],
+        lineWidth: 3,
+        pointSize: 5,
+        grid: true,
+        gridTextColor: '#697490',
+        gridTextSize: 12,
+        gridTextFamily: '"Encode Sans Narrow", sans-serif',
+        smooth: true
+    }).on('click', function(i, row){
+        console.log(i, row);
     });
 });
