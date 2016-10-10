dcf5272c941fff47c20c67820fe3ce9f93bdb228
Nurdin <spaikerkg@yahoo.com>
2016-10-10 17:26:49 +0600

    https://github.com/optdyn/hub/issues/1934 https://github.com/optdyn/hub/issues/1916 https://github.com/optdyn/hub/issues/1946
    Implemented user profile - isotope script
    Implemented user-profile friends and invitations
    Implemented modals: edit, pricing, write review, and part of edit product

---
+++ js/pages/bazaar-main.js
@@ -0,0 +1,27 @@
+$(window).load(function(){
+    var $container = $('.panel__body');
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
+    });
+
+    $('.tabular .item').click(function(){
+        $('.tabular .active').removeClass('active');
+        $(this).addClass('active');
+
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        return false;
+    });
+});
