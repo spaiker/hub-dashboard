b30f7cb65a0ccf742d16e02c03ff18230e30b144
Nurdin <spaikerkg@yahoo.com>
2016-10-22 02:02:58 +0600

    https://github.com/optdyn/hub/issues/1982 Enhanced slider script

--- js/pages/warroom.js
+++ js/pages/warroom.js
@@ -2,10 +2,17 @@ $('.js-toggle-like').click(function(){
     $(this).addClass('features-widget-icon_like');
 });
 $('.js-toggle-slide').click(function(){
-    $('.content-body__sidebar').toggleClass('content-body__sidebar-animation-in');
-    if('.content-body__sidebar'.hasClass('content-body__sidebar-animation-in')) {
-        $('.extra-widget-item').click(function(){
-            $('.content-body__sidebar').removeClass('content-body__sidebar-animation-in');
-        });
-    }
+    $('.content-body__sidebar').toggleClass('content-body__sidebar-animation-in')
+    $(this).toggleClass('extra-widget-toggle_rotate');
+});
+
+$('.extra-widget-item').click(function(){
+    $('.content-body__sidebar').removeClass('content-body__sidebar-animation-in')
+    $('.js-toggle-slide').removeClass('extra-widget-toggle_rotate');
+});
+
+$('[data-toggle="tab"]').tooltip({
+    trigger: 'hover',
+    placement: 'left',
+    animate: true,
 });
