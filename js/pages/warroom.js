2cbf005f596dc31cf17918f607326d6941170df8
Nurdin <spaikerkg@yahoo.com>
2016-10-22 00:16:46 +0600

    https://github.com/optdyn/hub/issues/1982 Implemented modals and slider effect

--- js/pages/warroom.js
+++ js/pages/warroom.js
@@ -1,3 +1,11 @@
 $('.js-toggle-like').click(function(){
     $(this).addClass('features-widget-icon_like');
 });
+$('.js-toggle-slide').click(function(){
+    $('.content-body__sidebar').toggleClass('content-body__sidebar-animation-in');
+    if('.content-body__sidebar'.hasClass('content-body__sidebar-animation-in')) {
+        $('.extra-widget-item').click(function(){
+            $('.content-body__sidebar').removeClass('content-body__sidebar-animation-in');
+        });
+    }
+});
