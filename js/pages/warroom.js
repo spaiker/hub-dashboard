bf6c6eff0b496a3cb879599f55c201b1a363434e
Nurdin <spaikerkg@yahoo.com>
2016-10-23 18:02:37 +0600

    https://github.com/optdyn/hub/issues/2058 added patterns to map of peers

--- js/pages/warroom.js
+++ js/pages/warroom.js
@@ -3,18 +3,18 @@ $('.js-toggle-like').click(function(){
 });
 $('.js-toggle-slide').click(function(){
     $('.content-body__sidebar').toggleClass('content-body__sidebar-animation-in');
-    $('.content-body__workspace').toggleClass('content-body__workspace_no-sidebar');
+    $('.content-body__workspace_warroom').toggleClass('content-body__workspace_no-sidebar');
     $(this).toggleClass('extra-widget-toggle_rotate');
 });
 
 $('.extra-widget-item').click(function(){
     $('.content-body__sidebar').removeClass('content-body__sidebar-animation-in');
-    $('.content-body__workspace').removeClass('content-body__workspace_no-sidebar');
+    $('.content-body__workspace_warroom').removeClass('content-body__workspace_no-sidebar');
     $('.js-toggle-slide').removeClass('extra-widget-toggle_rotate');
 });
 
 $('[data-toggle="tab"]').tooltip({
     trigger: 'hover',
     placement: 'left',
-    animate: true,
+    animate: true
 });
