193e064fb578f585207ae3c01c7da2bdc992d6d3
Nurdin <spaikerkg@yahoo.com>
2016-10-23 17:33:51 +0600

    https://github.com/optdyn/hub/issues/2058 added patterns to warroom

--- js/pages/warroom.js
+++ js/pages/warroom.js
@@ -2,12 +2,14 @@ $('.js-toggle-like').click(function(){
     $(this).addClass('features-widget-icon_like');
 });
 $('.js-toggle-slide').click(function(){
-    $('.content-body__sidebar').toggleClass('content-body__sidebar-animation-in')
+    $('.content-body__sidebar').toggleClass('content-body__sidebar-animation-in');
+    $('.content-body__workspace').toggleClass('content-body__workspace_no-sidebar');
     $(this).toggleClass('extra-widget-toggle_rotate');
 });
 
 $('.extra-widget-item').click(function(){
-    $('.content-body__sidebar').removeClass('content-body__sidebar-animation-in')
+    $('.content-body__sidebar').removeClass('content-body__sidebar-animation-in');
+    $('.content-body__workspace').removeClass('content-body__workspace_no-sidebar');
     $('.js-toggle-slide').removeClass('extra-widget-toggle_rotate');
 });
 
